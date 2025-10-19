862b8875556e9a29e16dd06d874e1f7901647c21
Christian Rickert <rc.login@icloud.com>
2025-10-18 22:00:21 -0600

    fix typo

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 /*
 This file is part of blaupause.
 
-zRpyright (C) 2025 The Regents of the University of Colorado
+Copyright (C) 2025 The Regents of the University of Colorado
 
 blaupause is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
